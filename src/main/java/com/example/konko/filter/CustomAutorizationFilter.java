f9d5fb0da7b1abc62629bd0d51fa551241b38004
Anthony4m <amoahanthony41@gmail.com>
2022-08-28 22:53:38 +0000

    added an authorization filter

--- src/main/java/com/example/konko/filter/CustomAutorizationFilter.java
+++ src/main/java/com/example/konko/filter/CustomAutorizationFilter.java
@@ -1,2 +1,57 @@
-package com.example.konko.filter;public class CustomAutorizationFilter {
+package com.example.konko.filter;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.example.konko.Utils.AuthFilter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@Slf4j
+public class CustomAutorizationFilter extends OncePerRequestFilter {
+    private AuthFilter authFilter = new AuthFilter();
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if(request.getServletPath().equals("/app/api/login") || request.getServletPath().equals("/api/app/refreshToken") ){
+            filterChain.doFilter(request,response);
+        }else {
+            String authoriztionHeader = request.getHeader(AUTHORIZATION);
+            if(authoriztionHeader != null && authoriztionHeader.startsWith("Bearer ")){
+                try {
+                   Map<String,Collection> subject =  authFilter.vaildateAuthToken(request,authoriztionHeader,response);
+                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(subject.get("username"),null,subject.get("authorities"));
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                    filterChain.doFilter(request,response);
+                }catch (Exception e){
+                log.error("Error Loggin in{}", e.getMessage());
+                response.setHeader("Error",e.getMessage());
+                response.setStatus(FORBIDDEN.value());
+                    Map<String,String> error = new HashMap<>();
+                    error.put("Error_Message", e.getMessage());
+                    response.setContentType(APPLICATION_JSON_VALUE);
+                    new ObjectMapper().writeValue(response.getOutputStream(),error);
+                }
+            }else {
+                filterChain.doFilter(request,response);
+            }
+        }
+    }
 }
