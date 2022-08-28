d45601ed2a1a1ef3f2a9a534ab1aa34afef2976d
Anthony4m <amoahanthony41@gmail.com>
2022-08-28 22:54:57 +0000

    Added authentication and authorization filter

--- src/main/java/com/example/konko/Security/WebSecurityConfig.java
+++ src/main/java/com/example/konko/Security/WebSecurityConfig.java
@@ -1,18 +1,30 @@
-package com.example.konko.Security;
+package com.example.konko.security;
 
 import com.example.konko.User.Service.UserService;
+import com.example.konko.User.UserRole;
 import com.example.konko.Utils.PasswordEncoder;
+import com.example.konko.filter.CustomAuthenticationFilter;
+import com.example.konko.filter.CustomAutorizationFilter;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @AllArgsConstructor
@@ -22,30 +34,39 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Lazy
     @Autowired
     private final UserService userService;
-    private final PasswordEncoder passwordEncoder;
+    private final BCryptPasswordEncoder passwordEncoder;
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
+        customAuthenticationFilter.setFilterProcessesUrl("/api/login");
         http
                 .csrf().disable()
                 .authorizeHttpRequests()
-                .antMatchers("/api/app*/**")
-                .permitAll()
+                .antMatchers("/api/app/register").permitAll()
+                .antMatchers("/app/api/login","/api/app/refreshToken").permitAll()
+                .antMatchers(GET,"/api/app/**")
+                .hasAnyAuthority(String.valueOf(UserRole.USER))
                 .anyRequest()
                 .authenticated().and()
                 .formLogin();
+        http.sessionManagement().sessionCreationPolicy(STATELESS);
+        http.addFilter(customAuthenticationFilter);
+        http.addFilterBefore(new CustomAutorizationFilter(), UsernamePasswordAuthenticationFilter.class);
 
     }
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(daoAuthenticationProvider());
+//        auth.authenticationProvider(daoAuthenticationProvider());
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
     }
 
     @Bean
     public DaoAuthenticationProvider daoAuthenticationProvider(){
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(passwordEncoder.bCryptPasswordEncoder());
+        provider.setPasswordEncoder(passwordEncoder);
         provider.setUserDetailsService(userService);
         return provider;
     }
